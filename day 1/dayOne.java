c425f40ff9ce6a2f45dec2bb82aef07322d4d12f
SpicyNachos03 <122487143+SpicyNachos03@users.noreply.github.com>
2024-12-01 23:13:17 -0500

    part two done for day 1

--- day 1/dayOne.java
+++ day 1/dayOne.java
@@ -9,23 +9,33 @@ public class dayOne {
             
             ArrayList<Integer> leftList = new ArrayList<>();
             ArrayList<Integer> rightList = new ArrayList<>();
-        
+            HashMap<Integer, Integer> rightMap = new HashMap<>();
             while (s.hasNext()) {
                 int left = s.nextInt();
                 int right = s.nextInt();
                 leftList.add(left);
                 rightList.add(right);
+                if (rightMap.containsKey(right)) {
+                    rightMap.put(right, rightMap.get(right) + 1);
+                } else {
+                    rightMap.put(right, 1);
+                }
             } // while
         
             Collections.sort(leftList);
             Collections.sort(rightList);
         
-            int totalDistance = 0;
+            int locationIdOne = 0;
+            int locationIdTwo = 0;
             for (int i = 0; i < leftList.size(); i++) {
-                totalDistance += Math.abs(leftList.get(i) - rightList.get(i));
+                locationIdOne += Math.abs(leftList.get(i) - rightList.get(i));
+                if (rightMap.containsKey(leftList.get(i))) {
+                    locationIdTwo += leftList.get(i) * rightMap.get(leftList.get(i));
+                } 
             } // for
         
-            System.out.println(totalDistance);
+            System.out.println("ID One: " + locationIdOne);
+            System.out.println("ID Two: " + locationIdTwo);
             s.close();
             
 
